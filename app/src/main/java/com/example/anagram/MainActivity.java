4c81fc86cd4f150c2f0be73c4f5327df54ef25f4
Mykola Butsko <mykolabutsko@gmail.com>
2025-03-14 22:00:55 +0200

    Chore: bind UI elements in MainActivity

--- app/src/main/java/com/example/anagram/MainActivity.java
+++ app/src/main/java/com/example/anagram/MainActivity.java
@@ -1,6 +1,30 @@
 package com.example.anagram;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    public EditText enterText;
+    public EditText enterSymbols;
+    public TextView yourResult;
+    public Button button;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        enterText = findViewById(R.id.enterTextForAnagram);
+        enterSymbols = findViewById(R.id.enterSymbolsToIgnore);
+        button = findViewById(R.id.convertButton);
+        yourResult = findViewById(R.id.yourAnagram);
+    }
 }
